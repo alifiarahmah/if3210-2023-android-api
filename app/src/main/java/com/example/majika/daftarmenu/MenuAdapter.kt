d9ee954e6e51ec162eeada49ac7d38bd89eaf15b
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2023-02-12 10:55:03 +0700

    feat: add logic for increase/decrease order button

--- app/src/main/java/com/example/majika/daftarmenu/MenuAdapter.kt
+++ app/src/main/java/com/example/majika/daftarmenu/MenuAdapter.kt
@@ -1,8 +1,11 @@
 package com.example.majika.daftarmenu
 
+import android.graphics.Color
+import android.provider.CalendarContract.Colors
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.majika.R
@@ -16,12 +19,16 @@ class MenuAdapter(private val list:ArrayList<MenuItem>): RecyclerView.Adapter<Me
         val soldView:TextView
         val descView:TextView
         val orderView: TextView
+        val decreaseButton: Button
+        val increaseButton: Button
         init {
             textView = view.findViewById(R.id.title)
             priceView = view.findViewById(R.id.price)
             soldView = view.findViewById(R.id.sold_number)
             descView = view.findViewById(R.id.description)
             orderView = view.findViewById(R.id.totalOrder)
+            decreaseButton = view.findViewById(R.id.decreaseOrderButton)
+            increaseButton = view.findViewById(R.id.increaseOrderButton)
         }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
@@ -46,6 +53,26 @@ class MenuAdapter(private val list:ArrayList<MenuItem>): RecyclerView.Adapter<Me
         holder.soldView.text = data.sold.toString() + " Terjual"
         holder.descView.text = data.description
         holder.orderView.text = order.toString()
+        holder.decreaseButton.isClickable = false
+        holder.decreaseButton.setBackgroundColor(Color.GRAY)
+        holder.decreaseButton.setOnClickListener {
+            var order =  Integer.parseInt(holder.orderView.text.toString()) - 1
+            if(order<0){
+                order = 0
+            }
+            holder.orderView.text = order.toString()
+            if(order<=0){
+                holder.decreaseButton.isClickable = false
+                holder.decreaseButton.setBackgroundColor(Color.GRAY)
+            }
+        }
+        holder.increaseButton.setBackgroundColor(Color.rgb(247,225,201))
+        holder.increaseButton.setOnClickListener {
+            holder.decreaseButton.isClickable = true
+            holder.decreaseButton.setBackgroundColor(Color.rgb(247,225,201))
+            val order =  Integer.parseInt(holder.orderView.text.toString()) + 1
+            holder.orderView.text = order.toString()
+        }
     }
 
     override fun getItemCount(): Int = list.size
