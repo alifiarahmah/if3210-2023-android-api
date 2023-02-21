28652691c56e2a97889bb3270b57db27aed6c7b0
Alifia Rahmah <13520122@std.stei.itb.ac.id>
2023-02-21 22:43:48 +0700

    feat(payment): total price observe cart CRUD

--- app/src/main/java/com/example/majika/ui/cart/CartFragment.kt
+++ app/src/main/java/com/example/majika/ui/cart/CartFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.majika.R
 import com.example.majika.cart.Cart
 import com.example.majika.cart.CartAdapter
@@ -60,6 +61,24 @@ class CartFragment : Fragment() {
         }
         binding.totalPrice.text = totalPrice.toString()
 
+        // if item in cart changed, update total price
+        adapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+            override fun onChanged() {
+                super.onChanged()
+                var totalCartPrice = 0
+                for (item in cart) {
+                    totalCartPrice += item.price!! * item.quantity!!
+                }
+                binding.totalPrice.text = totalCartPrice.toString()
+            }
+        })
+
+        // if cart is empty, hide checkout button
+        if (cart.isEmpty()) {
+            binding.checkoutButton.visibility = View.GONE
+        }
+
+        // if cart is not empty, show checkout button to payment fragment
         binding.checkoutButton.setOnClickListener {
             val paymentFragment = PaymentFragment()
             val transaction = requireActivity().supportFragmentManager.beginTransaction()
