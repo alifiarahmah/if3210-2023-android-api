813d4e854d16084381c4797b75118ec34df5a79d
Alifia Rahmah <13520122@std.stei.itb.ac.id>
2023-02-22 09:33:46 +0700

    feat(cart): add getInstance and refactor database

---
+++ app/src/main/java/com/example/majika/data/CartDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.majika.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.majika.cart.CartDao
+import com.example.majika.data.entity.Cart
+
+@Database(entities = [Cart::class], version = 1)
+abstract class CartDatabase : RoomDatabase() {
+    abstract fun cartDao(): CartDao
+
+    companion object {
+        private var INSTANCE: CartDatabase? = null
+
+        fun getInstance(context: Context): CartDatabase? {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(
+                    context.applicationContext,
+                    CartDatabase::class.java, "carts"
+                )
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .build()
+            }
+            return INSTANCE
+        }
+    }
+}
