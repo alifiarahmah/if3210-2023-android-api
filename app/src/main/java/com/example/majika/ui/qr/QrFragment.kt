023e3c2e5c5362e717d452e5470a42e11d42cce7
13520098 <13520098@std.stei.itb.ac.id>
2023-02-22 22:52:04 +0700

    fix: camera sudah disetup

--- app/src/main/java/com/example/majika/ui/qr/QrFragment.kt
+++ app/src/main/java/com/example/majika/ui/qr/QrFragment.kt
@@ -5,11 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import com.example.majika.R
 import com.example.majika.databinding.FragmentQrBinding
 
 class QrFragment : Fragment() {
+
+    /**
+     * Companion object
+     */
+    companion object {
+        private const val CAMERA_REQ_CODE = 69
+    }
+    /**
+     * Camera Request dan Code Scanner
+     */
+    private lateinit var qrScanner: CodeScanner
 
     private var _binding: FragmentQrBinding? = null
 
@@ -20,13 +40,35 @@ class QrFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         val qrViewModel =
             ViewModelProvider(this).get(QrViewModel::class.java)
 
         _binding = FragmentQrBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        /**
+         * Setup Camera Scanner
+         */
+        qrScanner = CodeScanner(this.requireContext(), binding.qrScannerCam)
+        qrScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.TWO_DIMENSIONAL_FORMATS
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.SINGLE
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+            decodeCallback = DecodeCallback {
+                TODO("SCAN QR CODE")
+            }
+            errorCallback = ErrorCallback {
+                TODO("Error Handling")
+            }
+            binding.qrScannerCam.setOnClickListener {
+                TODO("Rescan Code")
+            }
+        }
 
         return root
     }
